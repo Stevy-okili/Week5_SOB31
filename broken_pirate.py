47c564150c6fa57b8f31643de501fd6ffdaf2d4d
Stevy-okili <okilintounastevy@gmail.com>
2025-04-08 19:41:05 +0400

    Update broken_pirate.py

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,5 @@
-greeting = input("Hello, possible pirate! What's the password?)
-if greeting in ["Arrr!"):
+greeting = input("Hello, possible pirate! What's the password?") #stevy changes
+if greeting in ["Arrr!"]: # stevy remplace ] by  )
 	print("Go away, pirate.")
-elif
+else: #  for Stevy elif become else
 print("Greetings, hater of pirates!")
