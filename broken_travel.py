12df1468365fbbd359c4a81eed8c93ae9f1332b4
Stevy-okili <okilintounastevy@gmail.com>
2025-04-10 02:23:15 +0400

    Update broken_travel.py

--- broken_travel.py
+++ broken_travel.py
@@ -6,11 +6,11 @@
 # if he is from the distant past (before 1900),
 # the present era (1900-2020) or from the far future (beyond 2020).
 
-year = int.input("Greetings! What is your year of origin?" ))
+year = int.input("Greetings! What is your year of origin?" )) #stevy remplaces ' by "
 
-if year <= 1900:
-    print ("Woah, that's the past!")
-elif year 1900 < year < 2020:
+if year <= 1900:  # stevy added :
+    print ("Woah, that's the past!")  #stevy remplce ' by "
+elif year 1900 < year < 2020:  #stevy changes the > 1900 &&... by 1900< year...
     print ("That's totally the present!")
-else:
+else:  #stevy remplace elif by else 
     print ("Far out, that's the future!!")
